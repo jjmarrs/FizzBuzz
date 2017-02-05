787c6676e1a5f47793fcc2d86b7ff14afb06f8e5
JJ <jdmarrs738@gmail.com>
2017-02-04 21:16:38 -0500

    Add main file.

---
+++ FizzBuzz.py
@@ -0,0 +1,20 @@
+# Program prints numbers from 1 to 100
+# If the number is divisible by 3, print "Fizz"
+# If the number is divisible by 5, print "Buzz"
+# If the number is divisible by both 3 and 5, print "FizzBuzz"
+# Created by Jason Marrero
+
+
+def fizzbuzz():
+    numbers = list(range(1, 101))
+    for num in numbers:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
