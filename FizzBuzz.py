95200dc8dbe2fb5d065d874cb39687ae1fd5e3a4
JJ <jdmarrs738@gmail.com>
2017-02-04 21:18:25 -0500

    second commit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,7 +2,7 @@
 # If the number is divisible by 3, print "Fizz"
 # If the number is divisible by 5, print "Buzz"
 # If the number is divisible by both 3 and 5, print "FizzBuzz"
-# Created by Jason Marrero
+# Created by JJ Marrs 
 
 
 def fizzbuzz():
